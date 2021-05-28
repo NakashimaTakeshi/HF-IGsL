19697c4f793a85223f3f73e20871d8c28cd63a06
Lotfi El Hafi <lotfi.elhafi@gmail.com>
2021-05-28 12:04:07 +0900

    WIP: Set data folder structure

---
+++ catkin_ws/src/rgiro_spco2_slam/data/reset-spco2-slam-output.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -r ./output/test/*
+mkdir -p ./output/test/img/
+mkdir -p ./output/test/map/
+mkdir -p ./output/test/weight/
+mkdir -p ./output/test/particle/
+mkdir -p ./output/test/tmp/
