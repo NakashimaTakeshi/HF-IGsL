a90431c17823022b9119520dbf88d72558c765cb
Lotfi El Hafi <lotfi.elhafi@gmail.com>
2021-06-09 12:57:49 +0900

    Update '.gitignore' with output files of 'rgiro_spco2_slam'

--- catkin_ws/src/rgiro_spco2_slam/data/reset-spco2-slam-output.bash
+++ catkin_ws/src/rgiro_spco2_slam/data/reset-spco2-slam-output.bash
@@ -3,6 +3,6 @@
 rm -r ./output/test/*
 mkdir -p ./output/test/img/
 mkdir -p ./output/test/map/
-mkdir -p ./output/test/weight/
 mkdir -p ./output/test/particle/
 mkdir -p ./output/test/tmp/
+mkdir -p ./output/test/weight/
