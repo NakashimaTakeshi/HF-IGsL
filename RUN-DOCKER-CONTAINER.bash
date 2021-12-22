a2d631ebdd2be478c61758c6b8fef6ed0f3c945a
Lotfi El Hafi <lotfi.elhafi@gmail.com>
2021-12-22 12:00:41 +0900

    Add Serket ROS examples in 'serket_ros' ROS package with launch file

--- RUN-DOCKER-CONTAINER.bash
+++ RUN-DOCKER-CONTAINER.bash
@@ -68,7 +68,7 @@ case "$2" in
   docker exec -i -t ${DOCKER_CONTAINER} bash
   ;;
   ( "darknet_ros_default.launch" | \
-    "rgiro_chatter_default.launch" | \
+    "serket_ros_default.launch" | \
     "turtlebot3_gazebo_default.launch" | \
     "turtlebot3_rviz_default.launch" )
   docker exec -i -t ${DOCKER_CONTAINER} bash -i -c "source ~/TurtleBot3/docker/turtlebot3-devel/scripts/run-roslaunch-repeatedly.bash $2"
---
+++ catkin_ws/src/serket_ros/nodes/image_categorization.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function,unicode_literals, absolute_import
+import sys
+sys.path.append("../Serket/")
+
+import serket as srk
+import serket_ros as srkros
+import mlda
+import CNN
+import rospy
+
+def main():
+    rospy.init_node( "image_categorization" )
+
+    obs = srkros.ObservationImg( "image" )
+    cnn = CNN.CNNFeatureExtractor( fileames=None )
+    mlda1 = mlda.MLDA( 3, [1000] )
+
+    cnn.connect( obs )
+    mlda1.connect( cnn )
+
+    n = 0
+    while not rospy.is_shutdown():
+        print("***", n , "***")
+        obs.update()
+        cnn.update()
+        mlda1.update()
+        n += 1
+
+if __name__=="__main__":
+    main()
