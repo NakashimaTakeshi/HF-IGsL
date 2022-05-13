a637f577453ecd2f3e8334e95a5aeba64937fcf9
Lotfi El Hafi <lotfi.elhafi@gmail.com>
2022-05-13 17:34:01 +0900

    Add launch files and RViz layout for multi-robot SLAM demo

--- BRINGUP-TURTLEBOT3-ROBOT.bash
+++ BRINGUP-TURTLEBOT3-ROBOT.bash
@@ -8,7 +8,7 @@
 
 ################################################################################
 
-sshpass -p 'turtlebot' ssh -o StrictHostKeyChecking=no pi@turtlebot3-01.local 'bash -c "
+ssh -f -t -l pi turtlebot3-01.local 'bash -c "
 source /opt/ros/kinetic/setup.bash;
 source ~/catkin_ws/devel/setup.bash;
 export ROS_MASTER_URI=http://sr-station-04.local:11311;
@@ -17,12 +17,13 @@ export TURTLEBOT3_MODEL=waffle_pi;
 killall -9 roscore;
 killall -9 rosmaster;
 ROS_NAMESPACE=turtlebot3_01 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="turtlebot3_01" set_lidar_frame_id:="turtlebot3_01/base_scan" &
-sleep 10;
+sleep 5;
 ROS_NAMESPACE=turtlebot3_01 roslaunch turtlebot3_bringup turtlebot3_rpicamera.launch &
-sleep 10;
+sleep 5;
+echo "Done"
 "'
 
-sshpass -p 'turtlebot' ssh -o StrictHostKeyChecking=no pi@turtlebot3-02.local 'bash -c "
+ssh -f -t -l pi turtlebot3-02.local 'bash -c "
 source /opt/ros/kinetic/setup.bash;
 source ~/catkin_ws/devel/setup.bash;
 export ROS_MASTER_URI=http://sr-station-04.local:11311;
@@ -31,12 +32,13 @@ export TURTLEBOT3_MODEL=waffle_pi;
 killall -9 roscore;
 killall -9 rosmaster;
 ROS_NAMESPACE=turtlebot3_02 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="turtlebot3_02" set_lidar_frame_id:="turtlebot3_02/base_scan" &
-sleep 10;
+sleep 5;
 ROS_NAMESPACE=turtlebot3_02 roslaunch turtlebot3_bringup turtlebot3_rpicamera.launch &
-sleep 10;
+sleep 5;
+echo "Done"
 "'
 
-sshpass -p 'turtlebot' ssh -o StrictHostKeyChecking=no pi@turtlebot3-03.local 'bash -c "
+ssh -f -t -l pi turtlebot3-03.local 'bash -c "
 source /opt/ros/kinetic/setup.bash;
 source ~/catkin_ws/devel/setup.bash;
 export ROS_MASTER_URI=http://sr-station-04.local:11311;
@@ -45,7 +47,8 @@ export TURTLEBOT3_MODEL=waffle_pi;
 killall -9 roscore;
 killall -9 rosmaster;
 ROS_NAMESPACE=turtlebot3_03 roslaunch turtlebot3_bringup turtlebot3_robot.launch multi_robot_name:="turtlebot3_03" set_lidar_frame_id:="turtlebot3_03/base_scan" &
-sleep 10;
+sleep 5;
 ROS_NAMESPACE=turtlebot3_03 roslaunch turtlebot3_bringup turtlebot3_rpicamera.launch &
-sleep 10;
+sleep 5;
+echo "Done"
 "'
