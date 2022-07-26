5fe6457f5c3b5245d38954f84ffc99e2f050db5a
ZHENGYouwei <zheng.youwei@em.ci.ritsumei.ac.jp>
2022-07-26 17:55:28 +0900

    Update word publisher

--- catkin_ws/src/serket_ros/nodes/word_publisher.py
+++ catkin_ws/src/serket_ros/nodes/word_publisher.py
@@ -11,16 +11,19 @@ class WordPublisher ():
         # Initialize the room dict
         self.kitchenDict = {'1': 'i heard something crashing in the kitchen in the middle of the night', '2': 'mother is in the kitchen',
          '3': 'every morning she helps her mother to prepare breakfast in the kitchen', '4': 'there is a cooking table next to the refrigerator',
-         '5': 'they are talking in the kitchen', '6': 'we were cooking tempura at that time', '7': 'mother hummed to herself as she went about her cooking',
-         '8': 'she finished up lunch with coffee', '9': 'opening the refrigerator, I noticed the meat had spoiled', '10': 'nothing remained in the refrigerator'}
+         '5': 'they are talking in the kitchen', '6': 'we were cooking tempura at that time', 
+         '7': 'mother hummed to herself as she went about her cooking', '8': 'she finished up lunch with coffee',
+         '9': 'opening the refrigerator, I noticed the meat had spoiled', '10': 'nothing remained in the refrigerator'}
 
         self.livingRoomDict = {'1': 'the living room adjoins the dining room', '2': 'the living room in my new house is very large',
-         '3': 'father made our living room more spacious', '4': 'one Sunday morning George burst into the living room and said the following', '5': 'our living room is sunny',
-         '6': 'she put the blanket over the child sleeping on the sofa', '7': 'as he was tired, he was lying on the sofa with his eyes closed', '8': 'i am tired of watching television',
+         '3': 'father made our living room more spacious', '4': 'one Sunday morning George burst into the living room and said the following',
+         '5': 'our living room is sunny', '6': 'she put the blanket over the child sleeping on the sofa',
+         '7': 'as he was tired, he was lying on the sofa with his eyes closed', '8': 'i am tired of watching television',
          '9': 'my father usually watches television after dinner', '10': 'there is a dictionary on the desk'}
 
-        self.bedroomDict = {'1': 'i went up to my bedroom on tiptoe', '2': 'i have my own bedroom at home', '3': 'i found myself lying in my bedroom',
-         '4': 'i share a bedroom with my sister', '5': 'i like to have a full-length mirror in my bedroom', '6': 'to be a good child, you need to go to bed and get up early', 
+        self.bedroomDict = {'1': 'i went up to my bedroom on tiptoe', '2': 'i have my own bedroom at home',
+         '3': 'i found myself lying in my bedroom', '4': 'i share a bedroom with my sister',
+         '5': 'i like to have a full-length mirror in my bedroom', '6': 'to be a good child, you need to go to bed and get up early', 
          '7': 'i go to bed early at night', '8': "she put on her sister's jeans and looked in the mirror",
          '9': 'a good conscience is a soft pillow', '10': 'the alarm clock is ten minutes fast'}
 
@@ -31,16 +34,16 @@ class WordPublisher ():
          '9': 'i want you to return the book I lent you the other day', '10': 'did you pay for the book'}
         
         self.bookshelfDict = {'1': 'he has a large number of books on his bookshelf', '2': 'he fixed the bookshelf to the wall', 
-        '3': 'he made her a bookshelf', '4': 'i have a large number of books on my bookshelf', 
-        '5': 'our living room is sunny', '6': 'the bookshelf is built in', 
-        '7': 'carry these books back to the bookshelf', '8': 'my son has read every book on that shelf', 
-        '9': 'the other day I came across a book that you might like', '10': 'the teacher asked me which book I liked'}
+         '3': 'he made her a bookshelf', '4': 'i have a large number of books on my bookshelf', 
+         '5': 'our living room is sunny', '6': 'the bookshelf is built in', 
+         '7': 'carry these books back to the bookshelf', '8': 'my son has read every book on that shelf', 
+         '9': 'the other day I came across a book that you might like', '10': 'the teacher asked me which book I liked'}
 
         self.restAreaDict = {'1': 'they had a rest for a while', '2': 'we took a rest for a while', 
-        '3': 'i want to rest a little because all the homework is finished', '4': 'i share a bedroom with my sister', 
-        '5': 'i felt better after I took a rest', '6': 'my grandfather would often read and study at this desk', 
-        '7': 'i want to take a rest', '8': 'may I share this table with you', 
-        '9': 'it makes me feel cheerful', '10': 'some people relax by reading'}
+         '3': 'i want to rest a little because all the homework is finished', '4': 'i share a bedroom with my sister', 
+         '5': 'i felt better after I took a rest', '6': 'my grandfather would often read and study at this desk', 
+         '7': 'i want to take a rest', '8': 'may I share this table with you', 
+         '9': 'it makes me feel cheerful', '10': 'some people relax by reading'}
 
         self.pub = rospy.Publisher('serket_ros/word_publisher/word', String, queue_size=10)
 
