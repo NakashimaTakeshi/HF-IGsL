7316088c23e1126e536e25574c135b9dae527930
NakashimaTakeshi <nakashima.takeshi@em.ci.ritsumei.ac.jp>
2023-04-03 17:22:03 +0900

    Add bash script for experiment automation

---
+++ manage_experiments.bash
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+CURRENT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+RESULT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )'/catkin_ws/result/eval/'
+# RESULT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )'/catkin_ws/result/eval/movie/tmp/'
+MODEL_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )'/catkin_ws/src/ros_rssm/scripts/'
+VALDATA_PATH='dataset'
+# VALDATA_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )'/dataset'
+ANALYSIS_FILEPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )'/catkin_ws/utils/'
+
+echo ${MODEL_PATH}
+echo ${VALDATA_PATH}
+
+# RSSM_MODEL='RSSM_node_MRSSM.py' #RSSM_node.py
+# INTEGRATION_MODE='1.0'
+# echo ${INTEGRATION_MODE}
+
+# Delete the result file
+rm -rf ${RESULT_PATH}npy
+rm -rf ${RESULT_PATH}movie/tmp
+
+# $(find ${RESULT_PATH}npy/ -maxdepth 1 -name "*.npy" -type f -delete)
+# $(find ${RESULT_PATH}movie/tmp/ -maxdepth 1 -name "*.mp4" -type f -delete)
+
+for RSSM_MODEL in MCL RSSM_node_MRSSM RSSM_node ;do
+    for INTEGRATION_MODE in replace likelihood ;do
+        # Set the experiment parameters
+        if [ ${RSSM_MODEL} == 'MCL' ]; then
+            sed -i "s/resp.integration_mode = .../resp.integration_mode = 0.0/g" ${MODEL_PATH}/RSSM_node_MRSSM.py
+            INTEGRATION_MODE=none
+            RSSM_MODEL=RSSM_node_MRSSM
+        else
+            if [ ${INTEGRATION_MODE} == 'likelihood' ]; then
+                # echo "INTEGRATION_MODE = 1.0"
+                sed -i "s/resp.integration_mode = .../resp.integration_mode = 1.0/g" ${MODEL_PATH}/${RSSM_MODEL}.py
+                # sed -i "s/resp.integration_mode = 2.0/resp.integration_mode = ${INTEGRATION_MODE}/g" ${MODEL_PATH}/${MODEL}
+            elif [ ${INTEGRATION_MODE} == 'replace' ]; then
+                # echo "INTEGRATION_MODE = 2.0"
+                sed -i "s/resp.integration_mode = .../resp.integration_mode = 2.0/g" ${MODEL_PATH}/${RSSM_MODEL}.py
+            else
+                echo "INTEGRATION_MODE = likelihood or replace"
+                exit 1
+            fi
+        fi
+
+        # Execute the experiment
+        cd ${CURRENT_PATH}
+        bash start_experiment.bash ${VALDATA_PATH} ${RSSM_MODEL}.py
+
+        # Analyze the result data
+        # Rename the result file
+        NPY_PATH=${RESULT_PATH}/npy/${RSSM_MODEL}_${INTEGRATION_MODE}/
+        # mkdir -p ${NPY_PATH}
+        # mv ${RESULT_PATH}npy/*.npy ${NPY_PATH}
+
+        # cp ${ANALYSIS_FILEPATH}/analyse_result/* ${NPY_PATH}
+        # cd ${NPY_PATH}
+        # bash prepare_eval.bash
+        # bash make_graph.bash
+        # # rm ${RESULT_PATH}/result.npy
+
+
+        MOVIE_PATH=${RESULT_PATH}/movie/tmp/
+        mkdir -p ${MOVIE_PATH}
+        mv ${RESULT_PATH}movie/*mp4 ${MOVIE_PATH}   
+
+        cp ${ANALYSIS_FILEPATH}/join_mp4.bash ${MOVIE_PATH}
+        cp ${ANALYSIS_FILEPATH}/rename_mp4.bash ${MOVIE_PATH}
+        cd ${MOVIE_PATH}
+        bash rename_mp4.bash ${RSSM_MODEL}_${INTEGRATION_MODE}
+
+        #if RSSM_MODEL is MCL, break the loop        
+        if [ ${RSSM_MODEL} == 'MCL' ]; then
+            break
+        fi
+    done
+done
--- start_experiment.bash
+++ start_experiment.bash
@@ -10,22 +10,40 @@ IFS=$'\n';
 
 # Recursively loop through all bag files in the specified directory
 BAG_DIRECTORY=$1
+# BAGS=$(find ${BAG_DIRECTORY} -maxdepth 1 -name '*.bag' | awk '{print $0 " " substr($0,match($0,/[0-9]+/),RLENGTH)}' | sort -k2 -n | cut -f 1 -d " ")
+BAGS=$(find ${BAG_DIRECTORY} -maxdepth 1 -name '*.bag' | awk '{print $0 " " substr($0,match($0,/[0-9]+/))}' | sort -k2 -n | cut -f 1 -d " ")
+
+SAVE_DIR="/root/TurtleBot3/catkin_ws/result/eval/movie/"
+if [ ! -d ${SAVE_DIR} ]; then
+  mkdir ${SAVE_DIR}
+fi
+$(find ${SAVE_DIR} -maxdepth 1 -name "*.mp4" -type f -delete)
+
+RSSM_MODEL=$2
+# RSSM_MODEL="${RSSM_MODEL:- }"
+
+echo "RSSM_MODEL = ${RSSM_MODEL}"
+
 #Command -maxdepth 1 makes sure it only searches in the folder and not subfolders
-for bag in $(find ${BAG_DIRECTORY} -maxdepth 1 -name '*.bag'); 
+for bag in ${BAGS}; 
 do
 	echo "Processing bag file ${bag}"
 	INPUT_DATASET_FILE="${bag}"
 
     # record_all2.sh
-    for((i=0; i<20; i++)); 
+    for((i=0; i<1; i++)); 
     do
-        roslaunch ros_rssm rssm_amcl.launch file_1:=${bag} &
+        # rosclean purge -y
+        roslaunch ros_rssm rssm_amcl.launch file_1:=${bag} rssm_model:=${RSSM_MODEL} &
+        echo "roslaunch ros_rssm rssm_amcl.launch file_1:=${bag}"
+        # roslaunch ros_rssm rssm_amcl.launch file_1:=${bag} &
         sleep 6.0
         ROSLAUNCH_PID=${!}
         echo "roslaunch PID = ${ROSLAUNCH_PID}"
         movie_name=$(basename "${bag}" .bag)_${i}.mp4
         # https://ffmpeg.org/ffmpeg-all.html#x11grab
-        ffmpeg -r film -y -an -f x11grab -show_region 1 -draw_mouse 0 -video_size 1850x1020 -framerate film -i :1.0+70,60 "${movie_name}" &
+        # ffmpeg -r film -y -an -f x11grab -show_region 1 -draw_mouse 0 -video_size 1850x1020 -framerate film -i :1.0+70,60 "${SAVE_DIR}""${movie_name}" &
+        ffmpeg -r film -y -an -f x11grab -show_region 1 -draw_mouse 0 -loglevel 8 -video_size 1850x1020 -framerate film -i :1.0+70,60 "${SAVE_DIR}""${movie_name}" &
         
         wait $ROSLAUNCH_PID
         kill -TERM ${!}
