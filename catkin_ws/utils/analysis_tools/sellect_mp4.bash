d794523ae1ca6942fbb92c63c582b18c816b3f90
NakashimaTakeshi <nakashima.takeshi@em.ci.ritsumei.ac.jp>
2023-04-07 10:21:57 +0900

    bash file for making joint movie.

--- catkin_ws/utils/analysis_tools/sellect_mp4.bash
+++ catkin_ws/utils/analysis_tools/sellect_mp4.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 CURRENT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # if [ $# -eq 0 ]; then
@@ -14,27 +14,52 @@ model1_l_epi=${3:-0}
 model1_r_epi=${4:-0}
 model2_l_epi=${5:-0}
 model2_r_epi=${6:-0}
-echo "find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/${dataset}_/ && /_record_amcl_/ && /_${mcl_epi}_/'"
-File1=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '{/${dataset}_/ && /_record_amcl_/ && /_${mcl_epi}_/}')
-BAGS=$(find ${BAG_DIRECTORY} -maxdepth 1 -name '*.bag' | awk '{print $0 " " substr($0,match($0,/[0-9]+/))}' | sort -k2 -n | cut -f 1 -d " ")
 
+File0=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/'"${dataset}"'_/ && /record_amcl/ && /_'"${model1_l_epi}"'_/')
+echo ${File0}
+file_arr=(${File0})
+# echo ${#file_arr[@]}
+if [ ${#file_arr[@]} -ne 1 ]; then
+    echo "File 0 is not unique"
+    exit 1
+fi
+
+File1=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/'"${dataset}"'_/ && /RSSM_node/ && !/MRSSM/ && /likelihood/ && /_'"${model1_l_epi}"'_/')
 echo ${File1}
-if [ ${#File1[@]} -ne 1 ]; then
+file_arr=(${File1})
+# echo ${#file_arr[@]}
+if [ ${#file_arr[@]} -ne 1 ]; then
     echo "File 1 is not unique"
     exit 1
 fi
 
-File2=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/${dataset}_/ && /RSSM_node/ && !/MRSSM/ && /likelihood/ && /_${model1_l_epi}_/')
+File2=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/'"${dataset}"'_/ && /RSSM_node_MRSSM/ && /likelihood/ && /_'"${model2_l_epi}"'_/')
 echo ${File2}
-if [ ${#File2[@]} -ne 1 ]; then
+file_arr=(${File2})
+# echo ${#file_arr[@]}
+if [ ${#file_arr[@]} -ne 1 ]; then
     echo "File 2 is not unique"
     exit 1
 fi
 
+File3=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/'"${dataset}"'_/ && /RSSM_node/ && !/MRSSM/ && /replace/ && /_'"${model1_r_epi}"'_/')
+echo ${File3}
+file_arr=(${File3})
+# echo ${#file_arr[@]}
+if [ ${#file_arr[@]} -ne 1 ]; then
+    echo "File 3 is not unique"
+    exit 1
+fi
 
+File4=$(find ${CURRENT_PATH} -maxdepth 1 -name '*.mp4' | awk '/'"${dataset}"'_/ && /RSSM_node_MRSSM/ && /replace/ && /_'"${model2_r_epi}"'_/')
+echo ${File4}
+file_arr=(${File4})
+# echo ${#file_arr[@]}
+if [ ${#file_arr[@]} -ne 1 ]; then
+    echo "File 4 is not unique"
+    exit 1
+fi
 
 
 # 5 つの動画を横に並べる
-# do
-#     ffmpeg -y -i ${File1} -i ${i}${SUFFIX_1}.mp4 -i ${i}${SUFFIX_2}.mp4 -i ${i}${SUFFIX_3}.mp4 -i ${i}${SUFFIX_4}.mp4 -filter_complex "[0:v][1:v]hstack[top];[2:v][3:v]hstack[bottom];[top][bottom]vstack[all];[4:v]pad=iw+200:ih*2:color=white[ref];[ref][all]hstack" ${dataset}.mp4
-# done
+ffmpeg -y -i ${File1} -i ${File2} -i ${File3} -i ${File4} -i ${File0} -filter_complex "[0:v][1:v]hstack[top];[2:v][3:v]hstack[bottom];[top][bottom]vstack[all];[4:v]pad=iw+200:ih*2:color=white[ref];[ref][all]hstack" ${dataset}.mp4
